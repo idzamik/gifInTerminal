0f5e1f52cdbdcc9509ae22b04fba069fad8ebcec
idzamik <idzamikk@gmail.com>
2024-08-06 11:43:36 +0300

    дополнительные настройки

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import shutil
-import numpy as np
 from PIL import Image
 from PIL import ImageSequence
 import time
 import math
+import sys
 
-print('\n\n\n')
+print('\n\n')
 
-# print(w, h)
+ascii_type = int(sys.argv[1])
+file_name = sys.argv[2]
+
+if file_name == '_':
+    gif_name = 'gifs/' + '1' + '.gif'   # ЗДЕСЬ ПИСАТЬ НАЗВАНИЕ ФАЙЛА ВО ВТОРЫХ КОВЫЧКАХ
+else:
+    gif_name = 'gifs/' + file_name + '.gif'
 
 gif_name = 'gifs/' + '1' + '.gif'
 
-symbol_str = ' .:/|0'
-# symbol_str = '0QC71]|/:. '
-
+if ascii_type == 1:
+    symbol_str = ' .:/|0'
+elif ascii_type == 2:
+    symbol_str = '0QC71]|/:. '
+else:
+    print('Введите верные данные!')
+    sys.exit()
+    
 
 def get_pixel_to_symbol(frame, x, y):
     return symbol_str[int(math.floor(frame.getpixel((x, y)) // (255/len(symbol_str))-0.001))]
@@ -21,19 +32,14 @@ def get_pixel_to_symbol(frame, x, y):
 
 def frame_to_ascii(frame, w, h):
     frame = frame.convert('L')
-    # .rotate(90, expand=True)
-    ascii_frame = []
     ascii_str = ''
     width, height = frame.size
-    # height, width = h, w
     for y in range(height):
         for x in range(width):
             ascii_str += str(get_pixel_to_symbol(frame, x, y))
         ascii_str += '\n'
     print(ascii_str)
     time.sleep(0.1)
-
-
 
 
 with Image.open(gif_name) as img:
